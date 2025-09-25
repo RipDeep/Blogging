9ba3f7f92603471b5e35ae9e6337669227108dde
Deep Das <itsdeep890@gmail.com>
2025-09-25 12:14:18 +0530

    DEBUGGING

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
-
-
-const Blog = require('./models/blog')
+const Blog = require("./models/blog");
 
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
 
-const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URL)
-.then((e) => {
-  console.log("MongoDB is connected");
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((e) => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // fails fast, visible in logs
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(checkForAuthenticationCookie('token'));
-app.use(express.static(path.resolve('./public')))
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   const allBlog = await Blog.find({});
-  res.render("home",{
+  res.render("home", {
     user: req.user,
-    blogs: allBlog
+    blogs: allBlog,
   });
 });
 
